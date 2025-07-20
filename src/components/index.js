633f7a3e55aa09b79b5d695837f248f26f2d4b04
Raj Sri Gulshan <rajgulshan20022000@gmail.com>
2025-07-20 20:25:23 +0530

    Ligin & Signup Component Done

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./footer/Footer.jsx";
 import Container from "./Container/container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
+import Input from "./Input.jsx";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input
 }
