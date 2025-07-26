c8d9c59131c6f5d6c569580ab92dfe65a9686ec3
Raj Sri Gulshan <rajgulshan20022000@gmail.com>
2025-07-26 19:21:18 +0530

    all pages are completed

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,12 @@
+import { PostForm,Container } from "../components";
+
+function AddPost(){
+    return (
+        <div className="py-8">
+                <Container>
+                    <PostForm/>
+                </Container>
+        </div>
+    );
+}
+export default AddPost;
